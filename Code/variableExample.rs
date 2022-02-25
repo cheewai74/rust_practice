c78b7c2bf2ad4799e6828d227945cbfa863d33e4
moses <academicsnotes@gmail.com>
2022-02-25 23:14:06 +0800

    Commit code

---
+++ Code/variableExample.rs
@@ -0,0 +1,14 @@
+fn main(){
+    let x = 10;
+    println! ("x is {}", x);
+
+    let mut y = 20;
+    println! ("y is {}", y);
+
+    y = 40;
+    println!("y is {}", y);
+
+    // unsigned integer
+    let z: u8 = 255;
+    println!("z is {}", z)
+}
