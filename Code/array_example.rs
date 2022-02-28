7d9a3b1cfba9c8d48bf2a7af3ebb2310c8926a73
moses <academicsnotes@gmail.com>
2022-02-28 14:26:22 +0800

    Commit scripts

---
+++ Code/array_example.rs
@@ -0,0 +1,10 @@
+// let array_name = [valie_1, value_2.....,value_n]
+
+fn main(){
+
+    // Create an Array
+    let arr = [10,20,30,40,50];
+
+    // Show array
+    println!("The array is {:?}", arr)
+}
